eca5f65c5159e1a17ad6a7ef4c94225f33bfec8c
Kinkuraj Sahoo <kinkurajs@gmail.com>
2020-09-30 08:19:41 +0200

    Initial project set up
    Initial project set up

---
+++ src/main/java/com/holmessquad/holmessquad/HolmessquadApplication.java
@@ -0,0 +1,13 @@
+package com.holmessquad.holmessquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolmessquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolmessquadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holmessquad/holmessquad/HolmessquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holmessquad.holmessquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolmessquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
